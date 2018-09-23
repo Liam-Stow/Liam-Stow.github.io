2dcd235119588e5e2b7ed8c8b055b21040bf4a6f
Liam Stow <liam.stow@outlook.com>
2018-09-23 19:26:14 +1000

    Site is now cooler, still useless

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
+var img;
+
+function preload(){
+  img = loadImage('liam.png');
+}
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
+  background(44,48,52);
+  image(img, 50, 50, 80, 80);
+  imageMode(CENTER)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(127);
-  fill(0, 0, 255);
-  rect(100,200,75,150);
-  rect(200,300,200,300);
-  line(20, 20, 50, 50);
+  background(44,48,52);
+  fill(70, 130, 180);
+  image(img, mouseX, mouseY, 50, 50);
 }
