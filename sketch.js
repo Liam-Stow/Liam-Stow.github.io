01284794f82bda115ba57c96d4492a784053eaaa
Liam Stow <31067146+Liam-Stow@users.noreply.github.com>
2018-09-23 17:26:36 +1000

    Added test p5.js project

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(127);
+  fill(0, 0, 255);
+  rect(100,200,75,150);
+  rect(200,300,200,300);
+  line(20, 20, 50, 50);
+}
