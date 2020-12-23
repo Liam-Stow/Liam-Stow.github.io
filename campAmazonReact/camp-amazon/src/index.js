9d80524df0f46600524321404fbc0dece297113f
Liam Stow <liam.stow@outlook.com>
2020-12-23 23:52:18 +1100

    crappy running with eval

--- campAmazonReact/camp-amazon/src/index.js
+++ campAmazonReact/camp-amazon/src/index.js
@@ -5,12 +5,20 @@ import Sketch from 'react-p5'
 import './index.css'
 
 class RobotCanvas extends React.Component {
-    setup(p5, canvasParentRef) {
+
+    constructor(props) {
+        super(props);
+    }
+
+    setup = (p5, canvasParentRef) => {
+        console.log(this.props)
+
         let canvas = p5.createCanvas(600, 600).parent(canvasParentRef)
     }
 
-    draw(p5) {
+    draw = (p5) => {
         p5.background("rgb(200,200,200)")
+        eval(this.props.userCode())
     }
 
     render() {
@@ -21,17 +29,45 @@ class RobotCanvas extends React.Component {
 }
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditorReady: false
+        }
+    }
+    valueGetter = React.createRef();
+
+    handleEditorDidMount = (_valueGetter) => {
+        this.setState({ isEditorReady: true })
+        this.valueGetter.current = _valueGetter;
+    }
+
+    handleShowValue = () => {
+        alert(this.valueGetter.current());
+    }
+
+    getCode = () => {
+        if (this.state.isEditorReady) {
+            return this.valueGetter.current()
+        } else {
+            return ""
+        }
+    }
+
     render() {
         return (
             <React.Fragment >
+                <button onClick={this.handleShowValue} disabled={!this.state.isEditorReady}>Show</button>
                 <div className="flexbox-container">
                     <Editor
                         className="CodeEditor"
                         height="100vh"
                         width="50%"
                         theme="dark"
-                        language="javascript" />
-                    <RobotCanvas />
+                        language="javascript"
+                        editorDidMount={this.handleEditorDidMount} />
+                    <RobotCanvas userCode={this.getCode}/>
                 </div>
             </React.Fragment>
         );
