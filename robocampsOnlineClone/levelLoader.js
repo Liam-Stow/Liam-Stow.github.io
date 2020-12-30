4e9ec394790321f505e1f4e12fa7f8612158788e
Liam Stow <liam.stow@outlook.com>
2020-12-30 20:12:05 +1100

    cleaned indentations on level 12 + 13

--- robocampsOnlineClone/levelLoader.js
+++ robocampsOnlineClone/levelLoader.js
@@ -1370,165 +1370,159 @@ const levels = {
       "const TRAIL_LENGTH = 4000;\n" +
       "const COLOUR_SENSOR_DISTANCE = 23;\n" +
       "const IMAGE_URL = 'https://i.imgur.com/3mBQ7z2.png';\n" +
-      "const LVL4_MAP = 'https://i.imgur.com/FbyLemC.png';\n" +
       "var map_choice = ''\n" +
       "\n" +
       "function Robot(x, y, a, s) {\n" +
-      "    this.x = x;\n" +
-      "    this.y = y;\n" +
-      "    this.a = a;\n" +
-      "    this.size = s;\n" +
-      "    \n" +
-      "    drawMap();\n" +
-      "    this.history = [];\n" +
-      "    this.path = [];\n" +
-      "    this.pathDir = [];\n" +
+      "  this.x = x;\n" +
+      "  this.y = y;\n" +
+      "  this.a = a;\n" +
+      "  this.size = s;\n" +
+      "  \n" +
+      "  drawMap();\n" +
+      "  this.history = [];\n" +
+      "  this.path = [];\n" +
+      "  this.pathDir = [];\n" +
       "\n" +
-      "    this.hasBattery = function hasBattery() {\n" +
-      "        return this.path.length < MAX_PATH_LENGTH;\n" +
+      "  this.hasBattery = function hasBattery() {\n" +
+      "    return this.path.length < MAX_PATH_LENGTH;\n" +
+      "  }\n" +
+      "\n" +
+      "  this.detectColour = function detectColour(c) {\n" +
+      "    const at = get(x + cos(a) * COLOUR_SENSOR_DISTANCE, y + sin(a) * COLOUR_SENSOR_DISTANCE);\n" +
+      "    return at[0] == red(c) && at[1] == green(c) && at[2] == blue(c) && at[3] == alpha(c);\n" +
+      "  }\n" +
+      "  \n" +
+      "  this.move = function move(dist, turnRate) {\n" +
+      "    for (var i = 1; i <= abs(dist); i++) {\n" +
+      "      a += turnRate / 100;\n" +
+      "      x = x + cos(a) * 2 * Math.sign(dist);\n" +
+      "      y = y + sin(a) * 2 * Math.sign(dist);\n" +
+      "      this.path.push(createVector(x, y));\n" +
+      "      this.pathDir.push(Math.sign(dist));\n" +
+      "      if (x >= width || x <= 0 || y >= height || y <= 0 || this.path.length > MAX_PATH_LENGTH) {\n" +
+      "        return false;\n" +
+      "      }\n" +
       "    }\n" +
+      "    return true;\n" +
+      "  }\n" +
       "\n" +
-      "    this.detectColour = function detectColour(c) {\n" +
-      "        const at = get(x + cos(a) * COLOUR_SENSOR_DISTANCE, y + sin(a) * COLOUR_SENSOR_DISTANCE);\n" +
-      "        // print(at, c.toString());\n" +
-      "        return at[0] == red(c) && at[1] == green(c) && at[2] == blue(c) && at[3] == alpha(c);\n" +
+      "  this.render = function render(time) {\n" +
+      "    if (time < this.path.length) {\n" +
+      "      //robot\n" +
+      "      var p = this.path[time];\n" +
+      "      noStroke();\n" +
+      "      fill(255, 0, 0, 255);\n" +
+      "      ellipse(p.x, p.y, this.size / 3, this.size / 3);\n" +
+      "      \n" +
+      "      //trail\n" +
+      "      for (var i = 0; i < this.history.length; i = i + 2) {\n" +
+      "        var pos = this.history[i];\n" +
+      "        fill(255, 0, 0, 100);\n" +
+      "        ellipse(pos.x, pos.y, 8, 8);\n" +
+      "      }\n" +
+      "      \n" +
+      "      this.history.push(createVector(p.x, p.y));\n" +
+      "      \n" +
+      "      //remove old history\n" +
+      "      if (this.history.length > TRAIL_LENGTH) {\n" +
+      "        this.history.shift();\n" +
+      "      }\n" +
+      "      \n" +
+      "      translate(p.x, p.y);\n" +
+      "      var q = this.history[this.history.length - 2];\n" +
+      "      rotate(p.sub(q).mult(this.pathDir[time]).heading());\n" +
+      "      imageMode(CENTER);\n" +
+      "      image(img, 0, 0, this.size, this.size);\n" +
       "    }\n" +
-      "\n" +
-      "    this.move = function move(dist, turnRate) {\n" +
-      "        for (var i = 1; i <= abs(dist); i++) {\n" +
-      "            a += turnRate / 100;\n" +
-      "            x = x + cos(a) * 2 * Math.sign(dist);\n" +
-      "            y = y + sin(a) * 2 * Math.sign(dist);\n" +
-      "            this.path.push(createVector(x, y));\n" +
-      "            this.pathDir.push(Math.sign(dist));\n" +
-      "            if (x >= width || x <= 0 || y >= height || y <= 0 || this.path.length > MAX_PATH_LENGTH) {\n" +
-      "                return false;\n" +
-      "            }\n" +
-      "        }\n" +
-      "        return true;\n" +
-      "    }\n" +
-      "\n" +
-      "    this.render = function render(time) {\n" +
-      "        if (time < this.path.length) {\n" +
-      "            //robot\n" +
-      "            var p = this.path[time];\n" +
-      "            noStroke();\n" +
-      "            fill(255, 0, 0, 255);\n" +
-      "            ellipse(p.x, p.y, this.size / 3, this.size / 3);\n" +
-      "\n" +
-      "            //trail\n" +
-      "            for (var i = 0; i < this.history.length; i = i + 2) {\n" +
-      "                var pos = this.history[i];\n" +
-      "                fill(255, 0, 0, 100);\n" +
-      "                ellipse(pos.x, pos.y, 8, 8);\n" +
-      "            }\n" +
-      "\n" +
-      "            this.history.push(createVector(p.x, p.y));\n" +
-      "\n" +
-      "            //remove old history\n" +
-      "            if (this.history.length > TRAIL_LENGTH) {\n" +
-      "                this.history.shift();\n" +
-      "            }\n" +
-      "\n" +
-      "            translate(p.x, p.y);\n" +
-      "            var q = this.history[this.history.length - 2];\n" +
-      "            rotate(p.sub(q).mult(this.pathDir[time]).heading());\n" +
-      "            imageMode(CENTER);\n" +
-      "            image(img, 0, 0, this.size, this.size);\n" +
-      "        }\n" +
-      "    }\n" +
-      "    this.move(2, 0); // make sure the robot is drawn\n" +
+      "  }\n" +
+      "  this.move(2, 0); // make sure the robot is drawn\n" +
       "}\n" +
       "\n" +
       "function preload() {\n" +
-      "    img = loadImage(IMAGE_URL);\n" +
-      "    map_lvl4 = loadImage(LVL4_MAP);\n" +
-      "    map_choice = random(['red', 'purple'])\n" +
+      "  img = loadImage(IMAGE_URL);\n" +
+      "  map_lvl4 = loadImage(LVL4_MAP);\n" +
+      "  map_choice = random(['red', 'purple'])\n" +
       "}\n" +
       "\n" +
       "let t = 0;\n" +
       "\n" +
       "function draw() {\n" +
-      "    if (t < robot.path.length) {\n" +
-      "        drawMap();\n" +
-      "        robot.render(t);\n" +
-      "        t += 1;\n" +
-      "        redraw();\n" +
-      "    }\n" +
+      "  if (t < robot.path.length) {\n" +
+      "    drawMap();\n" +
+      "    robot.render(t);\n" +
+      "    t += 1;\n" +
+      "    redraw();\n" +
+      "  }\n" +
       "}\n" +
       "\n" +
       "function drawMap() {\n" +
-      "    createCanvas(600, 600);\n" +
-      "    rectMode(CENTER);\n" +
-      "\n" +
-      "    noStroke();\n" +
-      "\n" +
-      "    fill(color('white'));\n" +
-      "    rect(width / 2, height / 2, width, height)\n" +
-      "\n" +
-      "    if (map_choice === 'purple') {\n" +
-      "        fill(color('purple'));\n" +
-      "        rect(300, 100, 50, 50);\n" +
-      "\n" +
-      "        fill(color('green'));\n" +
-      "        rect(70, 100, 50, 50);\n" +
-      "    } else {\n" +
-      "        fill(color('red'));\n" +
-      "        rect(300, 100, 50, 50);\n" +
-      "\n" +
-      "        fill(color('green'));\n" +
-      "        rect(530, 100, 50, 50);\n" +
-      "    }\n" +
-      "\n" +
-      "\n" +
+      "  createCanvas(600, 600);\n" +
+      "  rectMode(CENTER);\n" +
+      "  \n" +
+      "  noStroke();\n" +
+      "  \n" +
+      "  fill(color('white'));\n" +
+      "  rect(width / 2, height / 2, width, height)\n" +
+      "  \n" +
+      "  if (map_choice === 'purple') {\n" +
+      "    fill(color('purple'));\n" +
+      "    rect(300, 100, 50, 50);\n" +
+      "    \n" +
+      "    fill(color('green'));\n" +
+      "    rect(70, 100, 50, 50);\n" +
+      "  } else {\n" +
+      "    fill(color('red'));\n" +
+      "    rect(300, 100, 50, 50);\n" +
+      "    \n" +
+      "    fill(color('green'));\n" +
+      "    rect(530, 100, 50, 50);\n" +
+      "  }\n" +
       "}\n" +
       "\n" +
       "function setup() {\n" +
-      "   robot = new Robot(300, 500, 4.7, 60);\n" +
-      "   \n" +
-      "   // Goal: Drive the robot into the green square\n" +
-      "   \n" +
-      "   /*\n" +
-      "   You will need to detect the colour that appears in front of the robot\n" +
-      "   to make a decision. If the box is purple the robot will need to drive\n" +
-      "   left, if the box is red it will need to go right.\n" +
-      "   \n" +
-      "   Remember the while(){...} loop and the robot.detectColour() function.\n" +
-      "   */\n" +
-      "   \n" +
-      "   // Your code here:\n" +
-      "   \n" +
+      "  robot = new Robot(300, 500, 4.7, 60);\n" +
+      "  \n" +
+      "  // Goal: Drive the robot into the green square\n" +
+      "  \n" +
+      "  /*\n" +
+      "  You will need to detect the colour that appears in front of the robot\n" +
+      "  to make a decision. If the box is purple the robot will need to drive\n" +
+      "  left, if the box is red it will need to go right.\n" +
+      "  \n" +
+      "  Remember the while(){...} loop and the robot.detectColour() function.\n" +
+      "  */\n" +
+      "  \n" +
+      "  // Your code here:\n" +
+      "  \n" +
       "}\n",
 
    13: "const MAX_PATH_LENGTH = 10000;\n" +
       "const TRAIL_LENGTH = 4000;\n" +
       "const COLOUR_SENSOR_DISTANCE = 23;\n" +
       "const IMAGE_URL = 'https://i.imgur.com/3mBQ7z2.png';\n" +
-      "const LVL4_MAP = 'https://i.imgur.com/FbyLemC.png';\n" +
       "var map_choice = ''\n" +
       "\n" +
       "function Robot(x, y, a, s) {\n" +
-      "this.x = x;\n" +
-      "this.y = y;\n" +
-      "this.a = a;\n" +
-      "this.size = s;\n" +
-      "\n" +
-      "drawMap();\n" +
-      "this.history = [];\n" +
-      "this.path = [];\n" +
-      "this.pathDir = [];\n" +
-      "\n" +
-      "this.hasBattery = function hasBattery() {\n" +
-      "    return this.path.length < MAX_PATH_LENGTH;\n" +
-      "}\n" +
-      "\n" +
-      "this.detectColour = function detectColour(c) {\n" +
+      "  this.x = x;\n" +
+      "  this.y = y;\n" +
+      "  this.a = a;\n" +
+      "  this.size = s;\n" +
+      "  \n" +
+      "  drawMap();\n" +
+      "  this.history = [];\n" +
+      "  this.path = [];\n" +
+      "  this.pathDir = [];\n" +
+      "  \n" +
+      "  this.hasBattery = function hasBattery() {\n" +
+      "     return this.path.length < MAX_PATH_LENGTH;\n" +
+      "  }\n" +
+      "  \n" +
+      "  this.detectColour = function detectColour(c) {\n" +
       "    const at = get(x + cos(a) * COLOUR_SENSOR_DISTANCE, y + sin(a) * COLOUR_SENSOR_DISTANCE);\n" +
-      "    // print(at, c.toString());\n" +
       "    return at[0] == red(c) && at[1] == green(c) && at[2] == blue(c) && at[3] == alpha(c);\n" +
-      "}\n" +
-      "\n" +
-      "this.move = function move(dist, turnRate) {\n" +
+      "  }\n" +
+      "  \n" +
+      "  this.move = function move(dist, turnRate) {\n" +
       "    for (var i = 1; i <= abs(dist); i++) {\n" +
       "    a += turnRate / 100;\n" +
       "    x = x + cos(a) * 2 * Math.sign(dist);\n" +
@@ -1536,78 +1530,78 @@ const levels = {
       "    this.path.push(createVector(x, y));\n" +
       "    this.pathDir.push(Math.sign(dist));\n" +
       "    if (x >= width || x <= 0 || y >= height || y <= 0 || this.path.length > MAX_PATH_LENGTH) {\n" +
-      "        return false;\n" +
+      "      return false;\n" +
       "    }\n" +
-      "    }\n" +
-      "    return true;\n" +
+      "  }\n" +
+      "  return true;\n" +
       "}\n" +
       "\n" +
       "this.render = function render(time) {\n" +
-      "    if (time < this.path.length) {\n" +
+      "  if (time < this.path.length) {\n" +
       "    //robot\n" +
       "    var p = this.path[time];\n" +
       "    noStroke();\n" +
       "    fill(255, 0, 0, 255);\n" +
       "    ellipse(p.x, p.y, this.size / 3, this.size / 3);\n" +
-      "\n" +
+      "    \n" +
       "    //trail\n" +
       "    for (var i = 0; i < this.history.length; i = i + 2) {\n" +
-      "        var pos = this.history[i];\n" +
-      "        fill(255, 0, 0, 100);\n" +
-      "        ellipse(pos.x, pos.y, 8, 8);\n" +
+      "      var pos = this.history[i];\n" +
+      "      fill(255, 0, 0, 100);\n" +
+      "      ellipse(pos.x, pos.y, 8, 8);\n" +
       "    }\n" +
-      "\n" +
+      "    \n" +
       "    this.history.push(createVector(p.x, p.y));\n" +
-      "\n" +
+      "    \n" +
       "    //remove old history\n" +
       "    if (this.history.length > TRAIL_LENGTH) {\n" +
-      "        this.history.shift();\n" +
+      "      this.history.shift();\n" +
       "    }\n" +
-      "\n" +
+      "    \n" +
       "    translate(p.x, p.y);\n" +
       "    var q = this.history[this.history.length - 2];\n" +
       "    rotate(p.sub(q).mult(this.pathDir[time]).heading());\n" +
       "    imageMode(CENTER);\n" +
       "    image(img, 0, 0, this.size, this.size);\n" +
       "    }\n" +
-      "}\n" +
-      "this.move(2, 0); // make sure the robot is drawn\n" +
+      "  }\n" +
+      "  this.move(2, 0); // make sure the robot is drawn\n" +
       "}\n" +
       "\n" +
       "function preload() {\n" +
-      "img = loadImage(IMAGE_URL);\n" +
-      "map_lvl4 = loadImage(LVL4_MAP);\n" +
-      "map_choice = random(['a', 'b', 'c'])\n" +
+      "  img = loadImage(IMAGE_URL);\n" +
+      "  map_lvl4 = loadImage(LVL4_MAP);\n" +
+      "  map_choice = random(['a', 'b', 'c'])\n" +
       "}\n" +
       "\n" +
       "let t = 0;\n" +
       "\n" +
       "function draw() {\n" +
-      "if (t < robot.path.length) {\n" +
+      "  if (t < robot.path.length) {\n" +
       "    drawMap();\n" +
       "    robot.render(t);\n" +
       "    t += 1;\n" +
       "    redraw();\n" +
-      "}\n" +
+      "  }\n" +
       "}\n" +
       "\n" +
       "function drawMap() {\n" +
-      "createCanvas(600, 600);\n" +
-      "rectMode(CENTER);\n" +
-      "\n" +
-      "noStroke();\n" +
-      "\n" +
-      "fill(color('white'));\n" +
-      "rect(width / 2, height / 2, width, height)\n" +
-      "\n" +
-      "if (map_choice === 'a') {\n" +
+      "  createCanvas(600, 600);\n" +
+      "  rectMode(CENTER);\n" +
+      "  \n" +
+      "  noStroke();\n" +
+      "  \n" +
+      "  fill(color('white'));\n" +
+      "  rect(width / 2, height / 2, width, height)\n" +
+      "  \n" +
+      "  if (map_choice === 'a') {\n" +
       "    fill(color('red'));\n" +
       "    rect(100, 100, 50, 50);\n" +
       "    rect(200, 100, 50, 50);\n" +
       "    rect(400, 400, 50, 50);\n" +
       "    rect(400, 300, 50, 50);\n" +
       "    rect(400, 200, 50, 50);\n" +
-      "\n" +
+      "    \n" +
       "    fill(color('purple'))\n" +
       "    rect(200, 500, 50, 50);\n" +
       "    rect(500, 500, 50, 50);\n" +
@@ -1619,8 +1613,8 @@ const levels = {
       "    \n" +
       "    fill(color('green'));\n" +
       "    rect(300, 400, 50, 50);\n" +
-      "\n" +
-      "} else if (map_choice === 'b') {\n" +
+      "    \n" +
+      "  } else if (map_choice === 'b') {\n" +
       "    fill(color('red'));\n" +
       "    rect(100, 400, 50, 50);\n" +
       "    rect(200, 300, 50, 50);\n" +
@@ -1629,7 +1623,7 @@ const levels = {
       "    rect(400, 400, 50, 50);\n" +
       "    rect(200, 200, 50, 50);\n" +
       "    rect(100, 200, 50, 50);\n" +
-      "\n" +
+      "    \n" +
       "    fill(color('purple'));\n" +
       "    rect(200, 400, 50, 50);\n" +
       "    rect(300, 300, 50, 50);\n" +
@@ -1640,40 +1634,38 @@ const levels = {
       "    rect(500, 200, 50, 50);\n" +
       "    rect(500, 100, 50, 50);\n" +
       "    rect(200, 100, 50, 50);\n" +
-      "\n" +
+      "    \n" +
       "    fill(color('green'));\n" +
       "    rect(100, 100, 50, 50);\n" +
       "    \n" +
-      "} else {\n" +
-      "      fill(color('red'))\n" +
-      "      rect(100, 300, 50, 50);\n" +
-      "      rect(300, 100, 50, 50);\n" +
-      "      \n" +
-      "      fill(color('purple'))\n" +
-      "      rect(300, 300, 50, 50);\n" +
-      "      \n" +
-      "      fill(color('green'))\n" +
-      "      rect(500, 100, 50, 50);\n" +
-      "   }\n" +
-      "\n" +
-      "\n" +
+      "  } else {\n" +
+      "    fill(color('red'))\n" +
+      "    rect(100, 300, 50, 50);\n" +
+      "    rect(300, 100, 50, 50);\n" +
+      "    \n" +
+      "    fill(color('purple'))\n" +
+      "    rect(300, 300, 50, 50);\n" +
+      "    \n" +
+      "    fill(color('green'))\n" +
+      "    rect(500, 100, 50, 50);\n" +
+      "  }\n" +
       "}\n" +
       "\n" +
       "function setup() {\n" +
-      "   robot = new Robot(100, 500, 4.7, 50);\n" +
-      "   \n" +
-      "   // Goal: Drive the robot into the green square\n" +
-      "   \n" +
-      "   /*\n" +
-      "   You will need to detect the coloured squares as the robot \n" +
-      "   detects them and use that to help solve the maze.\n" +
-      "   There are several differnt maps, so you can't just drive.\n" +
-      "   straight to the green square!\n" +
-      "   \n" +
-      "   You might want to use your function fromt he previous level.\n" +
-      "   */\n" +
-      "   \n" +
-      "   //Your code here:\n" +
-      "   \n" +
+      "  robot = new Robot(100, 500, 4.7, 50);\n" +
+      "  \n" +
+      "  // Goal: Drive the robot into the green square\n" +
+      "  \n" +
+      "  /*\n" +
+      "  You will need to detect the coloured squares as the robot \n" +
+      "  detects them and use that to help solve the maze.\n" +
+      "  There are several differnt maps, so you can't just drive.\n" +
+      "  straight to the green square!\n" +
+      "  \n" +
+      "  You might want to use your function fromt he previous level.\n" +
+      "  */\n" +
+      "  \n" +
+      "  // Your code here:\n" +
+      "  \n" +
       "}\n"
 }
